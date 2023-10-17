3d155fde505a9323a643af33ec0833c31aa78c0b
yedhu <yedhukrish700@gmail.com>
2023-10-17 18:13:00 +0530

    update2

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -176,9 +176,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'image/'
-MEDIA_ROOT = 'media/img/'
+MEDIA_ROOT = 'static/img/'
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticn/')
 
 # MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = 'media/'
